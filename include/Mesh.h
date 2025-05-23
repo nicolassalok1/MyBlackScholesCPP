fb63a8ca3e317a85b451a6ad5a9a82e43993aadf
salok1 <nicolas93pelletier@gmail.com>
2025-05-23 16:23:10 +0200

    main Payoff Option PDE FiniteDifference Mesh

--- include/Mesh.h
+++ include/Mesh.h
@@ -5,28 +5,44 @@
 #include <vector>
 
 /**
- * @brief Classe Mesh permettant de créer une discrétisation d'un domaine spatial ou temporel
+ * @brief Classe représentant un maillage uniforme sur un intervalle [0, a]
  */
 class Mesh {
-    private:
-        double* data;   // Tableau contenant la discrétisation temporelle
-        double a;       // Largeur de l'intervalle
-        int size;       // Nombre de subdivisions
-    public:
-        Mesh(double a_, int size_);
-        ~Mesh();
+private:
+    std::vector<double> data;  ///< Valeurs discrètes
+    double a;                  ///< Longueur de l'intervalle
+    int size;                  ///< Nombre de subdivisions (points - 1)
 
-    public:
-        double get_interval() const { return a; }
-        int get_size() const { return size; }
-        double get_step() const { return (double)(a / size); }
-        std::vector<double> get_vector();
+public:
+    /**
+     * @brief Construit un maillage uniforme
+     * @param a_ Longueur de l'intervalle
+     * @param size_ Nombre de subdivisions (M ⇒ M+1 points)
+     */
+    Mesh(double a_, int size_);
 
-    public:
-        double operator[](int i) const;
+    double get_interval() const { return a; }
+    int get_size() const { return size; }
 
-    public:
-        friend std::ostream& operator<<(std::ostream& st, const Mesh& m);
+    /**
+     * @brief Retourne le pas de discrétisation h = a / size
+     */
+    double get_step() const { return a / size; }
+
+    /**
+     * @brief Retourne toutes les valeurs sous forme de std::vector
+     */
+    std::vector<double> get_vector() const { return data; }
+
+    /**
+     * @brief Accès direct aux points via operator[]
+     */
+    double operator[](int i) const;
+
+    /**
+     * @brief Affichage du maillage
+     */
+    friend std::ostream& operator<<(std::ostream& os, const Mesh& m);
 };
 
 #endif
--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,77 +1,36 @@
 #include "Mesh.h"
-
 #include <stdexcept>
 
 /**
- * @brief Construction d'un objet Mesh (création d'une discrétisation)
- * 
- * @param a_ 
- * @param size_ 
+ * @brief Construit un maillage [0, a] avec size + 1 points
  */
 Mesh::Mesh(double a_, int size_) {
         if (size_ <= 0)
-                throw std::invalid_argument("Taille invalide");
-        // Initialisation des paramètres
-        size = size_ + 1;   // M intervalles ==> M + 1 bornes
+                throw std::invalid_argument("Taille invalide pour le maillage.");
+
         a = a_;
-        data = new double[size];
+        size = size_;
+        data.resize(size + 1);
 
-        // Vérification de l'allocation mémoire
-        if (!data)
-                throw "Echec de l'allocation de mémoire";
-
-        // Remplissage du tableau avec la discrétisation souhaitée
-        for (int i = 0; i < size; i++)
-                data[i] = i * ((double)a / (double)size);
-
-        // Message de statut
-        //std::cout << "Discrétisation ok" << std::endl;
+        double step = a / size;
+        for (int i = 0; i <= size; ++i)
+                data[i] = i * step;
 }
 
 /**
- * @brief Destructeur de la classe Mesh
- */
-Mesh::~Mesh() {
-        // Suppression du tableau de données
-        delete[] data;
-}
-
-/**
- * @brief Surcharge de l'opérateur [] (getter par index)
- * @param i Indice de l'objet à retourner
- * @return double
+ * @brief Accès à la i-ème valeur du maillage
  */
 double Mesh::operator[](int i) const {
-        if ((i < 0) || (i > size))
-                throw std::invalid_argument("Index invalide");
+        if (i < 0 || i > size)
+                throw std::out_of_range("Index hors limites dans Mesh.");
         return data[i];
 }
 
 /**
- * @brief Surcharge de l'opérateur <<
- * @param st Flux de sortie standard
- * @param m Objet de Mesh (discrétisation)
- * @return std::ostream& 
+ * @brief Affichage du maillage
  */
-std::ostream& operator<<(std::ostream& st, const Mesh& m) {
-        st << "[";
-        for (int i = 0; i < m.get_size(); i++) {
-                st << m[i];
-                if (i != (m.get_size() - 1))
-                        st << ";";
-        }
-        st << "]";
-        return st;
-}
-
-/**
- * @brief Fonction retournant une version std::vector<double> de la discrétisation
- * @return std::vector<double> 
- */
-std::vector<double> Mesh::get_vector() {
-        int n = size;
-        std::vector<double> res(n + 1, 0.0);
-        for (int i = 0; i < n; i++)
-                res[i] = data[i];
-        return res;
+std::ostream& operator<<(std::ostream& os, const Mesh& m) {
+        for (int i = 0; i <= m.size; ++i)
+                os << m.data[i] << " ";
+        return os;
 }
